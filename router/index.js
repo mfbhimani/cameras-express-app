c79ab5f056ae3ccf6d9e5c2b28157eecab9bbf52
Mohammed Bhimani <mfbhimani@gmail.com>
2019-05-26 13:17:24 -0400

    removed comments and old code

--- router/index.js
+++ router/index.js
@@ -13,52 +13,42 @@ router.get('/', (req, res) => {
 // route to handle registration of inputs: username and password
 
 router.post('/register', (req, res, next) => {
-  console.log(req.body);
   res.sendStatus(200);
 });
 
 // route to handle login with specific inputs provided: username and password
 
 router.post('/login', (req, res, next) => {
-  console.log(req.body);
   res.sendStatus(200);
 });
 
 // get list of cameras
 
 router.get('/camera', (req, res, next) => {
-  // res.json({msg: 'returns list of cameras'});
   res.sendStatus(200);
-  // res.status(200).json({msg:'return list of cameras'});
 });
 
 // get camera by ID
 
 router.get('/camera/:id', (req, res, next) => {
-  console.log(req.body);
-  // res.json({"msg2":`view camera of id ${req.params.id}`});
   res.sendStatus(200);
 });
 
 // create camera
 
 router.post('/camera', (req, res, next) => {
-  console.log(req.body);
-  // res.json({"msg2":`view camera of id ${req.params.id}`});
   res.sendStatus(200);
 });
 
 // updating camera by ID
 
 router.put('/camera/:id', (req, res, next) => {
-  console.log(req.body);
   res.sendStatus(200);
 });
 
 // deleting camera by ID
 
 router.delete('/camera/:id', async (req, res, next) => {
-  console.log(req.body);
   res.sendStatus(200);
 });
 
