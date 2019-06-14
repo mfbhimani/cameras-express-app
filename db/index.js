72400534f442cad7211b8b4f547a9d06933b3d1d
Mohammed Bhimani <mfbhimani@gmail.com>
2019-06-13 21:12:53 -0400

    db/index.js

--- db/index.js
+++ db/index.js
@@ -10,17 +10,36 @@ let writeFile = util.promisify(fs.writeFile);
 const filePath = path.resolve('db/cameras.json');
 
 async function readCameras() {
-  const json = await readFile(filePath);
+  const json = await readFile(filePath, 'utf8');
   return JSON.parse(json);
 }
 
 async function writeCameras(cameras) {
   const json = JSON.stringify(cameras, null, 2);
   await writeFile(filePath, json);
+
+  const json = JSON.stringify(items, null, 2);
+  return writeFile(dbfilepath, json, 'utf8')
 }
 
 async function createCamera(country) {
   const allCountries = await readCountries();
   await writeCountries(allCountries.concat(country));
+
+  items.push(newItem);
+  return writeitems(items);
 }
 
+asyc function getitembyid(id) {
+
+  const items = await readitems();
+
+  let matcheditem ;
+  items.forEach((item) => {
+    if (item.id === id) {
+      matcheditem = item ;
+    }
+  });
+
+  res.json(item);
+}
