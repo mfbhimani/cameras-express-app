9b6bdbdfc8eade0c313e21af0c3545b275d4e192
Mohammed Bhimani <mfbhimani@gmail.com>
2019-06-13 20:09:20 -0400

    updated server.js and db/index.js

--- db/index.js
+++ db/index.js
@@ -8,3 +8,19 @@ let readFile = util.promisify(fs.readFile);
 let writeFile = util.promisify(fs.writeFile);
 
 const filePath = path.resolve('db/cameras.json');
+
+async function readCameras() {
+  const json = await readFile(filePath);
+  return JSON.parse(json);
+}
+
+async function writeCameras(cameras) {
+  const json = JSON.stringify(cameras, null, 2);
+  await writeFile(filePath, json);
+}
+
+async function createCamera(country) {
+  const allCountries = await readCountries();
+  await writeCountries(allCountries.concat(country));
+}
+
